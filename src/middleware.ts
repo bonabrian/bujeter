5b151c4d820a7576575e766b1bc98c3de35ee921
bonabrian <bonabriansiagian@gmail.com>
2023-08-20 03:18:37 +0700

    feat: enhance middleware initialization

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,25 +2,31 @@ import { type NextRequest, NextResponse } from 'next/server'
 
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next()
-  const url = req.nextUrl
+  const { nextUrl } = req
 
   // TODO: will implement login session
-  const session = true
-  const isPublicPath = ['/auth/signin', '/auth/signup'].includes(url.pathname)
+  const hasSession = false
+  const isPublicPath = ['/auth/signin', '/auth/signup'].includes(
+    nextUrl.pathname,
+  )
 
-  if (isPublicPath && session) {
-    url.pathname = '/'
-    return NextResponse.redirect(url)
+  if (isPublicPath && hasSession) {
+    nextUrl.pathname = '/'
+    return NextResponse.redirect(nextUrl)
   }
 
-  if (!isPublicPath && !session) {
-    url.pathname = '/auth/signin'
-    return NextResponse.redirect(url)
+  if (!isPublicPath && !hasSession) {
+    return NextResponse.redirect(
+      new URL(
+        `/auth/signin?${new URLSearchParams({ next: nextUrl.pathname })}`,
+        nextUrl,
+      ),
+    )
   }
 
   return res
 }
 
 export const config = {
-  matchers: ['/((?!api/|_next/|_vercel|favicon.ico|sitemap.xml).*)'],
+  matcher: ['/', '/auth/signin', '/auth/signup'],
 }
